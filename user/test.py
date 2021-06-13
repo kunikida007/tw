0a035a4d4203c2b79f7ec85fcad278545a930fa0
Yuki Onodera <(maroonv4869@icloud.com)>
2021-06-13 12:53:14 +0900

    文法修正

--- user/test.py
+++ user/test.py
@@ -1,6 +1,6 @@
-from django.test import TestCase,Client
+from django.test import TestCase, Client
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.urls import reverse, 
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .views import signup
 from config import settings
@@ -14,62 +14,62 @@ class SigupTest(TestCase):
     def test_signup_view(self):
 
         response = self.client.get(self.index_url)
-        # URLを取得できるか。
+        
         self.assertEqual(response.status_code, 200)
-        #テンプレートを表示するか
+      
         self.assertTemplateUsed(response, 'user/signup.html')     
 
     def  test_post_fails_by_existed_user(self):
 
-         User.objects.create_user("username1", '', 'yy660818')
+        User.objects.create_user("username1", '', 'yy660818')
 
-         response = self.client.post(
-         self.index_url, 
-         {'username': 'username1', 'password1': 'yy660818', 'password2': 'yy660818'}
-    )
+        response = self.client.post(
+            self.index_url, 
+            {'username': 'username1', 'password1': 'yy660818', 'password2': 'yy660818'}
+        )
         
         # ページ遷移が発生していないか。   
-         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
             # テンプレートは正しいか。
-         self.assertTemplateUsed(response, 'user/signup.html')
+        self.assertTemplateUsed(response, 'user/signup.html')
          # エラーは表示されているか。
-         self.assertIn('このユーザーネームは既に使われています', response.context['form'].errors['username'])
+        self.assertIn('このユーザーネームは既に使われています', response.context['form'].errors['username'])
           # ユーザー名は既に存在するのか？
-         self.assertEqual(User.objects.filter(username='username1').count(), 1)
+        self.assertEqual(User.objects.filter(username='username1').count(), 1)
 
     def test_failed_with_common_password(self):
-         response = self.client.post(
-         self.index_url, 
-         {'username': 'username1', 'password1': 'password', 'password2': 'password'}
-    )
+        response = self.client.post(
+            self.index_url, 
+            {'username': 'username1', 'password1': 'password', 'password2': 'password'}
+         )
 
          # ページ遷移が発生していないか。   
-         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
             # テンプレートは正しいか。
-         self.assertTemplateUsed(response, 'user/signup.html')
+        self.assertTemplateUsed(response, 'user/signup.html')
          # エラーは表示されているか。
-         self.assertIn('このパスワードは一般的すぎます。', response.context['form'].errors["password2"])
+        self.assertIn('このパスワードは一般的すぎます。', response.context['form'].errors["password2"])
         
     def test_faild_with_diffrent_password(self):
 
         response = self.client.post(
-         self.index_url, 
-         {'username': 'username1', 'password1': 'yy660818', 'password2': 'yy660819'}
-    )
+            self.index_url, 
+            {'username': 'username1', 'password1': 'yy660818', 'password2': 'yy660819'}
+        )
          # エラーは表示されているか。
         self.assertIn('確認用パスワードが一致しません。', response.context['form'].errors["password2"])
 
     def test_failed_with_lack_password(self):
-         response = self.client.post(
-         self.index_url, 
-         {'username': 'username1', 'password1': '81y', 'password2': '81y'}
-    )
+        response = self.client.post(
+            self.index_url, 
+            {'username': 'username1', 'password1': '81y', 'password2': '81y'}
+        )
           # ページ遷移が発生していないか。   
-         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
          # テンプレートは正しいか。
-         self.assertTemplateUsed(response, 'user/signup.html')
+        self.assertTemplateUsed(response, 'user/signup.html')
          # エラーは表示されているか。
-         self.assertIn('このパスワードは短すぎます。最低 8 文字以上必要です。', response.context['form'].errors["password2"])
+        self.assertIn('このパスワードは短すぎます。最低 8 文字以上必要です。', response.context['form'].errors["password2"])
 
          
     def test_save_user(self):
@@ -110,17 +110,16 @@ class Login_test(TestCase):
 
     def test_with_correct_user(self):
 
-         User.objects.create_user("username1", '', 'yy660818')
-         data = {
+        User.objects.create_user("username1", '', 'yy660818')
+        data = {
             'username': "username1",
             'password': 'yy660818'
         }
-         response = self.client.post(reverse('user:login'), data=data)
+        response = self.client.post(reverse('user:login'), data=data)
 
          # ユーザー名は存在する。
-         self.assertEqual(User.objects.filter(username='username1').count(),1)
+        self.assertEqual(User.objects.filter(username='username1').count(),1)
          # ページ遷移が発生するか。   
-         self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
          #twiterのホーム画面に移動
-         self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
-    
+        self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
