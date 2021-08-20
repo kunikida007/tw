ddb0e15bed089d386fa67158e2f838c5b863f0a1
Yuki Onodera <(maroonv4869@icloud.com)>
2021-08-20 11:13:00 +0900

    いいね機能1

--- twiter/urls.py
+++ twiter/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
      path('tweet_detail/<int:pk>/', views.tweet_detail, name='tweet_detail'),
      path('delete_tweet/<int:pk>/', views.delete_tweet, name='delete_tweet'),
      path('accountpage/<int:user_id>/', views.accountpage, name='accountpage'),
+     path('favorite/<int:user_id>/<int:tweet_id>/', views.favorite, name='favorite'),
+     path('unfavorite/<int:user_id>/<int:tweet_id>/', views.unfavorite, name='unfavorite'),
+     path('tweet_favorite_detail/<int:pk>/', views.tweet_favorite_detail, name='tweet_favorite_detail'),
+
 ]
--- twiter/views.py
+++ twiter/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from user.models import Follow
+from .models import Favorite
 
 
 @login_required
@@ -38,8 +39,10 @@ def post_list(request):
 @login_required
 def tweet_detail(request, pk):
     tweet = get_object_or_404(models.Post, pk=pk)
+    favorited = models.Favorite.objects.filter(tweet=tweet, favorite_user=request.user).exists()
     context = {
         'tweet': tweet,
+        'favorited': favorited,
     }
     return render(request, 'twiter/tweet_detail.html', context)
 
@@ -63,3 +66,29 @@ def accountpage(request, user_id):
         'is_following': Follow.objects.filter(follower__username=request.user.username, following__username=user.username).exists(),
     }
     return render(request, 'twiter/account.html', context)
+
+
+@login_required
+def favorite(request, user_id, tweet_id):
+    tweet = get_object_or_404(models.Post, pk=tweet_id)
+    user = request.user
+    models.Favorite.objects.get_or_create(tweet=tweet, favorite_user=user)
+    return redirect('twiter:tweet_detail', tweet.pk)
+
+
+@login_required
+def unfavorite(request, user_id, tweet_id):
+    tweet = get_object_or_404(models.Post, pk=tweet_id)
+    user = request.user
+    favorite = get_object_or_404(Favorite, tweet=tweet, favorite_user=user)
+    favorite.delete()
+    return redirect('twiter:tweet_detail', tweet.pk)
+
+
+@login_required
+def tweet_favorite_detail(request, pk):
+    context = {
+        'pk': pk,
+        'favorite_user_list': models.Favorite.objects.filter(tweet__pk=pk).select_related('favorite_user').order_by('favorite_date')
+        }
+    return render(request, 'twiter/tweet_favorite_detail.html', context)
